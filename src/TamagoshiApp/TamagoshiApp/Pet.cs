7f7be7d773b7a8c05ee51c3a23e025b56996c431
Сергей Пчельник <Sergeypchelnik@mail.ru>
2024-03-11 16:58:21 +0700

    feature(all): реализовано тестовое задание, добавлен дополнителный параметр "happiness"

---
+++ src/TamagoshiApp/TamagoshiApp/Pet.cs
@@ -0,0 +1,107 @@
+﻿namespace TamagoshiApp
+{
+    public class Pet
+    {
+        private string _name;
+        private int _health;
+        private int _hunger;
+        private int _fatigue;
+        private int _happiness;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name can't be empty");
+                }
+                _name = value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                if(value == 0)
+                {
+                    _health = value;
+                    throw new ArgumentException("Your Pet is sick");
+                }
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("Value incorrect");
+                }
+                _health = value;
+            }
+        }
+
+        public int Hunger
+        {
+            get
+            {
+                return _hunger;
+            }
+            set
+            {
+                if( Hunger == 0 && value < Hunger)
+                {
+                    Health--;
+                    throw new ArgumentException("Be careful, your pet will overeat");
+                }
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("Value incorrect");
+                }
+                _hunger = value;
+            }
+        }
+
+        public int Fatigue
+        {
+            get => _fatigue;
+            set
+            {
+                if (Fatigue == 10 && value != 0)
+                {
+                    Hunger++;
+                    Health--;
+                    throw new ArgumentException("Be careful, your pet is tired");
+                }
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("Value incorrect");
+                }
+                _fatigue = value;
+            }
+        }
+
+        public int Happiness
+        {
+            get => _happiness;
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("Value incorrect");
+                }
+                _happiness = value;
+            }
+        }
+
+        public Pet(string name)
+        {
+            Name = name;
+            Health = 10;
+        }
+    }
+}
---
+++ src/TamagoshiApp/TamagoshiApp/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace TamagoshiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string petName;
+            string errorText = "";
+            Console.WriteLine("enter your pet's name");
+            petName = Console.ReadLine();
+            Pet pet = new Pet(petName);
+
+            while (true)
+            {
+                Console.Clear();
+                char borderChar = '*';
+                int borderLength = 30;
+                string formattedName = $"* {pet.Name} *";
+                Console.WriteLine(formattedName.PadLeft(borderLength / 2 + formattedName.Length / 2, borderChar)
+                        .PadRight(borderLength, borderChar));
+                Console.WriteLine($"Health: {new string('+', pet.Health)}");
+                Console.WriteLine($"Hunger: {new string('+', pet.Hunger)}");
+                Console.WriteLine($"Fatigue: {new string('+', pet.Fatigue)}");
+                Console.WriteLine($"Happiness: {new string('+', pet.Happiness)}");
+                Console.WriteLine(new string(borderChar, borderLength));
+                Console.WriteLine(errorText);
+                if (errorText == "Your Pet is sick")
+                {
+                    return;
+                }
+                errorText = "";
+                Console.WriteLine("Select action");
+                Console.WriteLine("1. Feed");
+                Console.WriteLine("2. Play");
+                Console.WriteLine("3. Sleep");
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Приложение завершено");
+                    return;
+                }
+                switch (keyInfo.KeyChar)
+                {
+                    case '1':
+                        {
+                            try
+                            {
+                                pet.Hunger--;
+                                break;
+                            }
+                            catch(ArgumentException e)
+                            {
+                                errorText = e.Message;
+                                break;
+                            }
+                        }
+                    case '2':
+                        try
+                        {
+                            pet.Fatigue++;
+                            pet.Happiness++;
+                            break;
+                        }
+                        catch (ArgumentException e)
+                        {
+                            errorText = e.Message;
+                            break;
+                        }
+                    case '3':
+                        try
+                        {
+                            pet.Fatigue = 0;
+                            pet.Health++;
+                            pet.Hunger++;
+                            break;
+                        }
+                        catch (ArgumentException e)
+                        {
+                            errorText = e.Message;
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
